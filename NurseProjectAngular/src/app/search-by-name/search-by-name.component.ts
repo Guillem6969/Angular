1cf4e8dae40b480a59bfe7f85b38d898345ac5ae
Guillem6969 <guim.san.oli@gmail.com>
2024-11-27 16:54:21 +0100

    search by name

--- NurseProjectAngular/src/app/search-by-name/search-by-name.component.ts
+++ NurseProjectAngular/src/app/search-by-name/search-by-name.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import jsonData from '../data/DATA.json';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search-by-name',
   templateUrl: './search-by-name.component.html',
   styleUrl: './search-by-name.component.css'
 })
+
 export class SearchByNAmeComponent {
 
+  users: any[] = jsonData;
+
+  searchName = new FormGroup({
+    name: new FormControl('')
+  });
+
+  searched: boolean = false;
+  nameSearched: String = ''; 
+
+  submitSearch(){
+    this.searched = false;
+    this.nameSearched = '';
+
+    this.users.forEach((user) => {
+      if(this.searchName.value.name === user.first_name){
+        this.searched = true;
+        this.nameSearched = user.first_name;
+      }
+    })
+
+    if(!this.searched){
+      console.log("NO ENCONTRADO")
+    }
+
+  }
 }
